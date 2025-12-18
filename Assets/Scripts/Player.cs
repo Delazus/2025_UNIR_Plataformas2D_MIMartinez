e9de776111cd5e5e9cd1eea9fa92cbc9cc8a6fc0
Delazus <48772508+Delazus@users.noreply.github.com>
2025-12-18 14:23:16 -0500

    Character Move, Shoot and Jump

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,115 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private Animator animator;
+    private Collider2D col;
+
+    [SerializeField] float walkSpeed = 3f;
+    [SerializeField] float jumpSpeed = 10f;
+
+    [Header("Ground")]
+    [SerializeField] LayerMask groundLayer;
+
+    [Header("Jump")]
+    [SerializeField] int maxJumps = 2; // doble salto
+
+    void Start()
+    {
+
+    }
+
+
+    int jumpsLeft;
+    bool wasGrounded;
+    void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        col = GetComponent<Collider2D>();
+
+        jumpsLeft = maxJumps;
+        wasGrounded = true;
+    }
+
+
+    Vector2 rawMove = Vector2.zero;
+    bool mustJump = false;
+    bool doShoot = false;
+
+    void Update()
+    {
+        UpdateRawMove();
+        Shoot();
+
+        // En el suelo
+        bool grounded = col.IsTouchingLayers(groundLayer);
+        animator.SetBool("IsGrounded", grounded);
+
+        // Resetea saltos
+        if (grounded)
+        { jumpsLeft = maxJumps; }
+        wasGrounded = grounded;
+
+        //Move
+        rb2D.linearVelocityX = rawMove.x * walkSpeed;
+
+        if (rawMove.x != 0)
+        {
+            animator.SetBool("IsWalking", true);
+            if (rawMove.x > 0) //Dcha.
+            {
+                transform.eulerAngles = Vector3.zero;
+            }
+            else //Izq.
+            { transform.eulerAngles = new Vector3(0, 180, 0); }
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
+
+        //Jump
+        if (mustJump && jumpsLeft > 0)
+        {
+            mustJump = false;
+            rb2D.linearVelocityY = jumpSpeed;
+            animator.SetTrigger("Jump");
+            jumpsLeft--;
+            wasGrounded = false;
+        }
+
+        //Shoot
+        if (doShoot)
+        {
+            doShoot = false;
+            if (animator.GetBool("IsWalking"))
+            { animator.SetTrigger("Shoot-Run"); }
+            else
+            { animator.SetTrigger("Shoot"); }
+        }
+
+    }
+    
+    private void UpdateRawMove()
+    {
+        rawMove = Vector2.zero;
+
+        if (Keyboard.current.aKey.isPressed)
+            { rawMove += Vector2.left; }
+        else if (Keyboard.current.dKey.isPressed)
+            { rawMove += Vector2.right; }
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame) 
+            { mustJump = true; }
+    }
+
+    private void Shoot()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        { doShoot = true; }
+    }
+}
